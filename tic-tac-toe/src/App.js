b1cf9e41b5adf32e6d38c7ecc38e3ac260a111ae
Niklas Furberg <niklas.furberg@gmail.com>
2020-09-09 13:48:04 +0200

    Added cell component

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Cell = () => {
+  const [value, setValueState] = useState('');
+  return (
+    <div className="cell" onClick={() => makeMove(setValueState)}>{value}</div>
+  );
+}
+
+const makeMove = (setCellValueState) => {
+  setCellValueState('X');
+}
+
+const App = () => {
   return (
     <div className="App">
       <div id="board">
-        <div class="cell">X</div>
-        <div class="cell">O</div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
+        {Array(9)
+          .fill()
+          .map((_, i) => (
+            <Cell key={i}/>
+          ))}
       </div>
     </div>
   );
