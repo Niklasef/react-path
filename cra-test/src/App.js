37e0338f950532679af72263fe8fb4ce8859db1d
Niklas Furberg <niklas.furberg@gmail.com>
2020-09-11 13:57:39 +0200

    Recreated cra app and added face-api demo

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {
+  detectSingleFace,
+  loadSsdMobilenetv1Model,
+  SsdMobilenetv1Options
+} from 'face-api.js';
+
 function App() {
+  const [videoElement, setVideoElement] = React.useState(null);
+
+  React.useEffect(() => {
+    setVideoElement(document.getElementById('video'));
+
+    if (videoElement === null) {
+      console.log("VideoElement doesn't exist");
+      return;
+    }
+    navigator
+      .mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          width: { ideal: 720 },
+          height: { ideal: 560 },
+        },
+      })
+      .then((stream) => {
+        videoElement.srcObject = stream;
+        startDetection();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    loadSsdMobilenetv1Model('/models')
+      .then(() => {
+        console.log('model loaded');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video
+        style={{ opacity: 1 }}
+        id='video'
+        width='720'
+        height='560'
+        autoPlay={true}
+        muted={true}></video>
     </div>
   );
+}
+
+async function startDetection() {
+  const videoElement = document.getElementById('video');
+  setInterval(async () => {
+    const detection = await
+      detectSingleFace(
+        videoElement,
+        new SsdMobilenetv1Options({ minConfidence: 0.1 }));
+
+    if (detection == null) {
+      console.log('detection is null');
+      return;
+    }
+    if (detection._score > 0.5) {
+      console.log('Found face');
+    }
+    else {
+      console.log('Didn´t find face');
+    }
+    console.log('Detection score: ' + detection._score.toFixed(2));
+  }, 500);
 }
 
 export default App;
